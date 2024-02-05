c6e31c7e407096b43e6082a71a82fd3e1ee6f0ab
sherwinmanosa <145818525+sherwinmanosa@users.noreply.github.com>
2024-02-05 22:19:32 +0800

    Header hero (#11)
    * sherwin's update
    
    * Sherwin's update 050224
    
    * sherwin's update - change SVG url
    
    * Sherwin update 0020524
    
    * Update class name for common btn
    
    * Remove space

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+
+import './js/mobile-menu';
+
 import './js/mobile-menu'; // removed unused
+
 import './js/modal';
 // import './js/smooth-scroll';
 import './js/screen-scroll';
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,5 +1,3 @@
-  //For mobile menu
-
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
@@ -7,7 +5,8 @@
   const body = document.body;
 
   const toggleMenu = () => {
-    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
