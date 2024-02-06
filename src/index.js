5cfb29c10011b7c208b6590762440809d43855b0
sherwinmanosa <sherwin.manosa@gmail.com>
2024-02-06 14:17:16 +0800

    update js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-
 import './js/mobile-menu';
-
-import './js/mobile-menu'; // removed unused
-
 import './js/modal';
-// import './js/smooth-scroll';
 import './js/screen-scroll';
-// import './js/swiper-review'
+import './js/swiper-review';
+import './js/smooth-scroll';
