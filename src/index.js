464c9af82a0666b3e28116891dc43234da3fa7e2
sherwinmanosa <sherwin.manosa@gmail.com>
2024-02-06 17:39:00 +0800

    removed duplicate (scroll and swiper)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import './js/mobile-menu';
 import './js/modal';
 import './js/screen-scroll';
-
-import './js/swiper-review';
-import './js/smooth-scroll';
-
 // import './js/swiper-bundle.js';
 import './js/swiper-review';
 // import './js/smooth-scroll'; // missing plugin
