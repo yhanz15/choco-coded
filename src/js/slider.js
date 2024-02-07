91c677461826d15ead072600003cd016a4e0a2ab
marylou rubis martirez <148078790+lotickles@users.noreply.github.com>
2024-02-07 18:40:20 +0800

    fixed chocolate is loved (#29)

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,19 +1,38 @@
-   var swiper = new Swiper(".mySwiper", {
-                    slidesPerView: 1,
-                    spaceBetween: 18,
-     loop: true,
-                    pagination: {
-                        el: ".swiper-pagination",
-                        clickable: true,
-                  },
-        breakpoints: {
-            768: {
-                slidesPerView: 2.5,
-                spaceBetween: 18,
-            },
-            1024: {
-                slidesPerView: 4,
-                spaceBetween: 18,
-            },
-        },
-   });
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 18,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 18,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 18,
+    },
+  },
+});
+
+var swiper = new Swiper('.swiper-loved', {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
+    },
+  },
+});
